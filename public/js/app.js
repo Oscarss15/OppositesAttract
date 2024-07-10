f93e89a9941a70cbb247b24c9a434f3b625c717a
Oscar <oscar15-91@hotmail.com>
2024-07-10 09:34:07 +0200

    añadidos test

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-function isLove(petalsFlower1, petalsFlower2) {
+export function isLove(petalsFlower1, petalsFlower2) {
   if (petalsFlower1 % 2 === 0 && petalsFlower2 % 2 === 1) {
     return true;
   } else if (petalsFlower1 % 2 === 1 && petalsFlower2 % 2 === 0) {
@@ -7,6 +7,3 @@ function isLove(petalsFlower1, petalsFlower2) {
     return false;
   }
 }
-petals1 = Math.floor(Math.random() * 10) + 1;
-petals2 = Math.floor(Math.random() * 10) + 1;
-isLove(petals1, petals2);
